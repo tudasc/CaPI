29b144f9670b4b7945bc230f381064b75cff036c
Sebastian Kreutzer <SebastianKreutzer@gmx.net>
2022-03-22 17:05:32 +0100

    Add ScoreP support for XRay instrumentation

--- cmake/ToolchainOptions.cmake
+++ cmake/ToolchainOptions.cmake
@@ -15,7 +15,16 @@ include(clang-format)
 include(llvm-util)
 include(talp)
 
+option(SCOREP_SUPPORT "Enable Score-P support" ON)
 
+if (SCOREP_SUPPORT)
+  set(SCOREP_PATH "" CACHE PATH "Path to ScoreP installation")
+
+  find_library(scorep_mgmt  NAMES scorep_adapter_compiler_mgmt HINTS "${SCOREP_PATH}/lib"  REQUIRED)
+  find_library(scorep_measurement  NAMES scorep_measurement HINTS "${SCOREP_PATH}/lib" REQUIRED)
+
+  message(STATUS "Score-P libs: ${scorep_mgmt} ${scorep_measurement}")
+endif()
 
 
 if (NOT CMAKE_BUILD_TYPE)
@@ -26,7 +35,7 @@ endif ()
 
 if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   # set default install path
-  set(CMAKE_INSTALL_PREFIX "${instro-llvm_SOURCE_DIR}/install/instro-llvm" CACHE PATH "Default install path" FORCE)
+  set(CMAKE_INSTALL_PREFIX "${capi_SOURCE_DIR}/install/capi" CACHE PATH "Default install path" FORCE)
   message(STATUS "Installing to (default): ${CMAKE_INSTALL_PREFIX}")
 endif ()
 
--- lib/xray/XRayRuntime.h
+++ lib/xray/XRayRuntime.h
@@ -11,39 +11,6 @@
 
 namespace capi {
 
-using SymbolTable = std::map<std::uintptr_t, std::string>;
-
-struct MemMapEntry {
-  std::string path;
-  uintptr_t addrBegin;
-  uint64_t offset;
-};
-
-struct MappedSymTable {
-  SymbolTable table;
-  MemMapEntry memMap;
-};
-
-// TODO: This is copied from ScoreP Symbol Injector - use a commom source base
-class SymbolRetriever {
-  std::string execFile;
-
-  std::map<uintptr_t, MappedSymTable> addrToSymTable;
-  std::vector<std::pair<std::string, MappedSymTable>> symTables;
-
-  bool loadSymTables();
-
-public:
-  SymbolRetriever(std::string execFile);
-
-  bool run();
-
-  const std::map<uintptr_t, MappedSymTable>& getMappedSymTables() const {
-    return addrToSymTable;
-  }
-
-};
-
 
 void initXRay();
 
