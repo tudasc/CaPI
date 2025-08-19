0cc3fbac56c88a9cb050c4000e83ca972a54d7bd
Sebastian Kreutzer <SebastianKreutzer@gmx.net>
2025-08-19 18:28:16 +0200

    Adjust basic test and improve talp test

--- test/passes/002_basic_talp.cpp
+++ test/passes/002_basic_talp.cpp
@@ -10,14 +10,12 @@
 
 // (1) Check if region instrumentation is inserted correctly.
 // Note: Using dummy home, since mpic++ fails within lit without it
-// RUN: env HOME=/tmp mpic++ --showme:compile
 // RUN: env HOME=/tmp %capi_cc --capi-interface=talp_static --verbose mpic++ -O2  -mllvm -inst-filter="$(dirname %s)/001_basic.filt" -S -emit-llvm %s -o - | FileCheck %s
 
 // (2) Run with TALP, check if region statistics are emitted.
 // RUN: env HOME=/tmp %capi_cc --capi-interface=talp_static --verbose mpic++ -O2  -mllvm -inst-filter="$(dirname %s)/001_basic.filt" %talp_args %s -o %s.o
 // RUN: env HOME=/tmp DLB_ARGS="--talp --talp-summary=pop-metrics" mpirun -n 2 %s.o 2>&1 | FileCheck %s -check-prefix=CHECK-TALP
 
-// CHECK-TALP: DLB{{.*}} ### Name: MPI Execution
 // CHECK-TALP: DLB{{.*}} ### Name: _Z3fooi
 // CHECK-TALP-NOT: DLB{{.*}} ### Name: _Z3bari
 
@@ -31,12 +29,12 @@ int __attribute__((noinline)) bar(int x) {
 }
 
 // CHECK: define dso_local {{.*}} i32 @_Z3fooi
-// CHECK:  %0 = call ptr @DLB_MonitoringRegionRegister(ptr @0)
-// CHECK-NEXT:  %1 = call i32 @DLB_MonitoringRegionStart(ptr %0)
-// CHECK-NEXT:  %call = tail call noundef i32 @_Z3bari(i32 noundef %x)
-// CHECK-NEXT:  %add = add nsw i32 %call, 5
-// CHECK-NEXT:  %2 = call i32 @DLB_MonitoringRegionStop(ptr %0)
-// CHECK-NEXT:  ret i32 %add
+// CHECK:  [[R0:%.*]] = call ptr @DLB_MonitoringRegionRegister(ptr @0)
+// CHECK-NEXT:  [[R1:%.*]] = call i32 @DLB_MonitoringRegionStart(ptr [[R0]])
+// CHECK-NEXT:  [[CALL:%.*]] = tail call noundef i32 @_Z3bari
+// CHECK-NEXT:  [[ADD:%.*]] = add nsw i32 [[CALL]], 5
+// CHECK-NEXT:  [[R2:%.*]] = call i32 @DLB_MonitoringRegionStop(ptr [[R0]])
+// CHECK-NEXT:  ret i32 [[ADD]]
 // CHECK-NEXT: }
 int __attribute__((noinline))  foo(int x) {
   return bar(x) + 5;
