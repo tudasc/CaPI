affc12eea6ade86064d5e508583a58d3e18d4baf
Peter Arzt <peter.arzt@tu-darmstadt.de>
2025-09-01 13:52:02 +0200

    Move `traverseCallGraph` into TraversalHelper.h

--- include/capi/selection/TraversalHelper.h
+++ include/capi/selection/TraversalHelper.h
@@ -11,6 +11,41 @@
 namespace capi {
 
 
+/**
+ * Traverses the call graph, calling the given visit function on each node.
+ * @tparam TraverseFn Function that takes a metacg::CgNode& argument and returns the next
+ * nodes to traverse.
+ * @tparam VisitFn Function that takes a metacg::CgNode& argument.
+ * @param node
+ * @param visit
+ * @returns The number of visited functions.
+ */
+template <typename TraverseFn, typename VisitFn>
+int traverseCallGraph(const metacg::CgNode &node, TraverseFn &&selectNextNodes,
+                      VisitFn &&visit) {
+  std::vector<const metacg::CgNode *> workingSet;
+  std::unordered_set<const metacg::CgNode *> alreadyVisited;
+
+  workingSet.push_back(&node);
+
+  do {
+    auto currentNode = workingSet.back();
+    workingSet.pop_back();
+    //        std::cout << "Visiting caller " << currentNode->getName() << "\n";
+    visit(*currentNode);
+    alreadyVisited.insert(currentNode);
+    for (auto &nextNode : selectNextNodes(*currentNode)) {
+      if (!alreadyVisited.contains(nextNode)
+          && std::find(alreadyVisited.begin(), alreadyVisited.end(), nextNode) == alreadyVisited.end()) {
+        workingSet.push_back(nextNode);
+      }
+    }
+
+  } while (!workingSet.empty());
+
+  return alreadyVisited.size();
+}
+
 inline bool isNodeDestructor(const metacg::CgNode& node) {
   auto name = node.getFunctionName();
   return name.substr(0, 2) == "_Z" && !(name.compare(name.length() - 4, 4, "D0Ev")
--- src/selection/selectors/CallPathSelector.h
+++ src/selection/selectors/CallPathSelector.h
@@ -31,44 +31,6 @@ public:
   }
 };
 
-/**
- * Traverses the call chain downwards, calling the given visit function on each
- * node.
- * @tparam VisitFn Function that takes a metacg::CgNode& argument and returns the next
- * nodes to traverse.
- * @tparam VisitFn Function that takes a metacg::CgNode& argument.
- * @param node
- * @param visit
- * @returns The number of visited functions.
- */
-template <typename TraverseFn, typename VisitFn>
-int traverseCallGraph(const metacg::CgNode &node, TraverseFn &&selectNextNodes,
-                      VisitFn &&visit) {
-  std::vector<const metacg::CgNode *> workingSet;
-  std::vector<const metacg::CgNode *> alreadyVisited;
-
-  workingSet.push_back(&node);
-
-  do {
-    auto currentNode = workingSet.back();
-    workingSet.pop_back();
-    //        std::cout << "Visiting caller " << currentNode->getName() << "\n";
-    visit(*currentNode);
-    alreadyVisited.push_back(currentNode);
-    for (auto &nextNode : selectNextNodes(*currentNode)) {
-      if (std::find(workingSet.begin(), workingSet.end(), nextNode) ==
-          workingSet.end() &&
-          std::find(alreadyVisited.begin(), alreadyVisited.end(), nextNode) ==
-          alreadyVisited.end()) {
-        workingSet.push_back(nextNode);
-      }
-    }
-
-  } while (!workingSet.empty());
-
-  return alreadyVisited.size();
-}
-
 template <TraverseDir Dir> FunctionSet CallPathSelector<Dir>::apply(const FunctionSetList& input) {
   static_assert(Dir == TraverseDir::TraverseDown ||
                 Dir == TraverseDir::TraverseUp);
